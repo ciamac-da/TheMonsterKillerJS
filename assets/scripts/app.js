7b4cd3f0cc99e5ca910cbd5f298e4d08f092015c
ciamac-da <ciamac.davoudi@gmail.com>
2020-08-26 11:37:59 +0200

    Verified by Cia v1.1.5 ADDING CONDITIONAL BATTLE LOG

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,14 +5,21 @@ const MONSTER_ATTACK_VALUE = 10;
 const HEAL_VALUE = 17; 
 
 
-const MODE_ATTACK = "ATTACK" //MODE_ATTACK = 0
-const MODE_STRONG_ATTACK = "STRONG_ATTACK" //STRONG_ATTACK = 1
+const MODE_ATTACK = "ATTACK"; //MODE_ATTACK = 0
+const MODE_STRONG_ATTACK = "STRONG_ATTACK"; //STRONG_ATTACK = 1
+const LOG_EVENT_PLAYER_ATTACK = "PLAYER_ATTACK";
+const LOG_EVENT_PLAYER_STRONG_ATTACK = "PLAYER_STRONG_ATTACK";
+const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACK";
+const LOG_EVENT_PLAYER_HEAL = "PLAYER_HEAL";
+const LOG_EVENT_GAME_OVER = "GAME_OVER";
+
 
 
 const enteredValue = prompt("Maximum life for you and the monster", "100"); 
 
 //let chosenMaxLife = 100;
 let chosenMaxLife = parseInt(enteredValue);
+let battleLog = [];
 
 if(isNaN(chosenMaxLife || chosenMaxLife <= 0 || chosenMaxLife > 100)){
   chosenMaxLife = 100;
@@ -22,6 +29,46 @@ let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
 adjustHealthBars(chosenMaxLife);
+
+
+function writeToLog(ev, val,  monsterHealth, playerHealth ){
+  let logEntry;
+  if(ev === LOG_EVENT_PLAYER_ATTACK){
+ logEntry = {
+   event: ev,
+   value: val,
+   target: "MONSTER",
+   finalMonsterHealth: monsterHealth,
+   finalPlayerHealth: playerHealth
+ };
+  } else if(ev === LOG_EVENT_PLAYER_STRONG_ATTACK){
+    logEntry = {
+      event: ev,
+      value: val,
+      target: "MONSTER",
+      finalMonsterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth
+    };
+  } else if(ev === LOG_EVENT_PLAYER_HEAL){
+    logEntry = {
+      event: ev,
+      value: val,
+      target: "PLAYER",
+      finalMonsterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth
+    };
+  } else if(ev === LOG_EVENT_GAME_OVER){
+    logEntry = {
+      event: ev,
+      value: val,
+      finalMonsterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth
+    };
+  }
+  battleLog.push(logEntry);
+}
+
+
 
 
 function reset(){
