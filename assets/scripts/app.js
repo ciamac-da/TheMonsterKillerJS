f1fc30362214c9d088ad004d0d3e18e0e80de44c
ciamac-da <ciamac.davoudi@gmail.com>
2020-08-26 10:37:58 +0200

    Verified by Cia v1.1.3 choose life adjusted

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,8 +3,15 @@ const STRONG_ATTACK_VALUE = 25;
 const MONSTER_ATTACK_VALUE = 10; 
 //const STRONG_MONSTER_ATTACK_VALUE = 20; 
 const HEAL_VALUE = 17; 
+const enteredValue = prompt("Maximum life for you and the monster", "100"); 
 
-let chosenMaxLife = 100;
+//let chosenMaxLife = 100;
+let chosenMaxLife = parseInt(enteredValue);
+
+if(isNaN(chosenMaxLife || chosenMaxLife <= 0 || chosenMaxLife > 100)){
+  chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
